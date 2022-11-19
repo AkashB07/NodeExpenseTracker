b87247e8ac20e2b32944261c2763ce52d788033d
AkashB07 <akashbadhi7@gmail.com>
2022-11-19 13:27:03 +0530

    login front end

---
+++ Login/login.js
@@ -0,0 +1,20 @@
+async function login(e)
+{
+    try 
+    {
+        e.preventDefault();
+        const loginDetails = {
+            email: e.target.email.value,
+            password: e.target.password.value
+        }
+        console.log(loginDetails);
+
+        const respone = await axios.post('http://localhost:3000/user/login', loginDetails)
+        alert(respone.data.message)
+    }
+    catch (err) 
+    {
+        console.log(JSON.stringify(error))
+        document.body.innerHTML += `<div style="color:red;">${err.message}<div>`;
+    }
+}
