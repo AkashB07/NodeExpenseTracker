72597d4bb85b71f801201d63f22e3fa5d57b2693
AkashB07 <akashbadhi7@gmail.com>
2022-11-21 13:42:12 +0530

    forgot password frontend

--- Login/login.js
+++ Login/login.js
@@ -20,3 +20,7 @@ async function login(e)
         document.body.innerHTML += `<div style="color:red;">${err.message}<div>`;
     }
 }
+
+function forgotpassword() {
+    window.location.href = "../ForgotPassword/password.html"
+}
