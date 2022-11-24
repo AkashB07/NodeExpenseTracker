4c1854106216e19cb94d4373396b7cf005f5a949
AkashB07 <akashbadhi7@gmail.com>
2022-11-24 20:57:15 +0530

    leaderboard

--- leaderboard/leaderboard.js
+++ leaderboard/leaderboard.js
@@ -22,20 +22,20 @@ window.addEventListener('DOMContentLoaded',async ()=>{
             `
 
             user_cont=  document.getElementById('use_cont')
-        const alluser = await axios.get(`http://localhost:3000/user/getallusers`,{ headers: {"Authorization" : token}})
-        let paramString = window.location.href;
-        console.log(paramString.split('/'))
-        console.log(alluser.data)
-        var data=alluser.data
-        data.sort((a, b) => parseFloat(b.totalexpense) - parseFloat(a.totalexpense));
-        for(let i=0;i<data.length;i++){
-        user_cont.innerHTML=user_cont.innerHTML+`<div class="users" id="users">
-        <p>${i+1}</p>
-        <p><a href='./user.html?userid=${data[i].id}'>${data[i].name}</a></p>
-        <p>${data[i].totalexpense}</p>
-        </div>`
+            const alluser = await axios.get(`http://localhost:3000/user/getallusers`,{ headers: {"Authorization" : token}})
+            let paramString = window.location.href;
+            console.log(paramString.split('/'))
+            console.log(alluser.data)
+            var data=alluser.data
+            data.sort((a, b) => parseFloat(b.totalexpense) - parseFloat(a.totalexpense));
+            for(let i=0;i<data.length;i++){
+                user_cont.innerHTML=user_cont.innerHTML+`<div class="users" id="users">
+                <p>${i+1}</p>
+                <p><a href='./user.html?userid=${data[i].id}'>${data[i].name}</a></p>
+                <p>${data[i].totalexpense}</p>
+                </div>`
 
-       }
+            }
        }
     } 
     catch (err) {
